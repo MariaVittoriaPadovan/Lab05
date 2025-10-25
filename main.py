1c8d70fd7a577f9f7b2232341c4fd056837c5a87
MariaVittoriaPadovan <mariavittoriapadovan9@gmail.com>
2025-10-25 16:18:51 +0200

    programma finito

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import flet as ft
 from alert import AlertManager
-from automobile import Automobile
 from autonoleggio import Autonoleggio
 
 FILE_AUTO = "automobili.csv"
@@ -39,10 +38,9 @@ def main(page: ft.Page):
     # Tutti i TextField per le info necessarie per aggiungere una nuova automobile (marca, modello, anno, contatore posti)
     # TODO
     #creo 3 campi TextField, non metto value perché devo aggiungere una nuova automobile
-    input_marca=ft.TextField(label='Marca')
+    input_marca= ft.TextField(label='Marca')
     input_modello = ft.TextField(label="Modello")
     input_anno= ft.TextField(label="Anno")
-
 
 
     # --- FUNZIONI APP ---
@@ -68,16 +66,40 @@ def main(page: ft.Page):
     # TODO
     #questo è il testo per gestire il counter,slide 18 pwp7
     def handleAdd(e):
-        currentVal = txtOut.value
-
+        currentVal = int(txtOut.value)
         txtOut.value = currentVal + 1
         txtOut.update()
 
     def handleRemove(e):
-        currentVal = txtOut.value
-
+        currentVal = int(txtOut.value)
         txtOut.value = currentVal - 1
         txtOut.update()
+
+    def conferma_automobile(e):
+        try:
+            marca=input_marca.value
+            modello=input_modello.value
+            anno=int(input_anno.value)
+            posti=int(txtOut.value)
+
+            if not marca or not modello:
+                raise ValueError("Inserisci marca e modello validi")
+
+            if anno < 1885 or anno > 2025 or posti==0 or posti>=8: #1885 anno prima macchina
+                raise ValueError("Inserisci valori numerici validi per anno e posti")
+
+            autonoleggio.aggiungi_automobile(marca, modello, anno, posti)
+
+            #svuoto i campi
+            input_marca.value = ""
+            input_modello.value = ""
+            input_anno.value = ""
+
+            aggiorna_lista_auto()
+            page.update()
+
+        except Exception as e:
+            alert.show_alert(f"❌ Errore: {e}")
 
     # --- EVENTI ---
     toggle_cambia_tema = ft.Switch(label="Tema scuro", value=True, on_change=cambia_tema)
@@ -87,7 +109,7 @@ def main(page: ft.Page):
     # TODO
     #crea i bottoni + e - del counter, slide 19 pwp 7
     btnMinus = ft.IconButton(icon=ft.Icons.REMOVE,
-                             icon_color="green",
+                             icon_color="red",
                              icon_size=24, on_click=handleRemove)
     btnAdd = ft.IconButton(icon=ft.Icons.ADD,
                            icon_color="green",
@@ -95,9 +117,9 @@ def main(page: ft.Page):
     txtOut = ft.TextField(width=100, disabled=True,
                           value=0, border_color="green",
                           text_align=ft.TextAlign.CENTER)
-    row1 = ft.Row([btnMinus, txtOut, btnAdd],
-                  alignment=ft.MainAxisAlignment.CENTER)
-    page.add(row1)
+
+    pulsante_conferma_automobile = ft.ElevatedButton("Aggiungi Automobile", on_click=conferma_automobile)
+
 
     # --- LAYOUT ---
     page.add(
@@ -117,6 +139,13 @@ def main(page: ft.Page):
         # Sezione 3
         # TODO
         ft.Text("Aggiungi Nuova Automobile ", size=20),
+        ft.Row(spacing=20,
+               controls=[input_marca, input_modello, input_anno, btnMinus, txtOut, btnAdd],
+               alignment=ft.MainAxisAlignment.CENTER),
+
+        ft.Row(spacing=0,
+               controls=[pulsante_conferma_automobile],
+               alignment=ft.MainAxisAlignment.CENTER),
 
         # Sezione 4
         ft.Divider(),
